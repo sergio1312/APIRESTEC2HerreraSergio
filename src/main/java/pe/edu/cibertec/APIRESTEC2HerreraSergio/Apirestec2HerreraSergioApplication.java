76afb076698c9de5f709e0d9a0122fe0c62c5122
Sergio <serham_95@hotmail.com>
2024-04-27 11:35:50 -0500

    examen cl2 DAW api

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2HerreraSergio/Apirestec2HerreraSergioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2HerreraSergio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestec2HerreraSergioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestec2HerreraSergioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/APIRESTEC2HerreraSergio/Apirestec2HerreraSergioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2HerreraSergio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestec2HerreraSergioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
